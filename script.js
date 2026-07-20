322df801ec9add5d4dc797b022d6f15f95609fd2
royrohitkumar665-lab <royrohitkumar665@gmail.com>
2026-07-20 15:03:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-f
+const palette = document.getElementById("palette");
+
+for (let i = 1; i <= 100; i++) {
+    const btn = document.createElement("button");
+    btn.innerText = i;
+    btn.className = "palette-btn";
+    btn.onclick = () => {
+        alert("Question " + i + " will open here.");
+    };
+    palette.appendChild(btn);
+}
+
+// Show student details
+document.getElementById("studentName").innerText =
+localStorage.getItem("studentName") || "Student";
+
+document.getElementById("studentRoll").innerText =
+localStorage.getItem("studentRoll") || "0000";
