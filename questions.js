a38377f667d2cd240049903ecd0bbfd309578b74
royrohitkumar665-lab <royrohitkumar665@gmail.com>
2026-07-21 10:44:04 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,22 @@
-q
+const questions = [
+{
+question: "भारत की राजधानी क्या है?",
+options: ["मुंबई", "कोलकाता", "नई दिल्ली", "चेन्नई"],
+answer: 2
+},
+{
+question: "2 + 2 = ?",
+options: ["3", "4", "5", "6"],
+answer: 1
+},
+{
+question: "RRB का पूर्ण रूप क्या है?",
+options: [
+"Railway Recruitment Board",
+"Road Recruitment Board",
+"Rail Recruitment Bureau",
+"Railway Road Board"
+],
+answer: 0
+}
+];
