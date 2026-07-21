1aafe7e4ba6afa9226fec6b630e81825814e2ff6
royrohitkumar665-lab <royrohitkumar665@gmail.com>
2026-07-21 21:12:03 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,32 @@
-q
+// =========================
+// Rohit Exam Portal
+// Home Page
+// =========================
+
+function studentLogin(){
+
+    let name = document.getElementById("studentNameInput").value.trim();
+    let roll = document.getElementById("studentRollInput").value.trim();
+
+    if(name === "" || roll === ""){
+        alert("Please enter your Name and Roll Number.");
+        return;
+    }
+
+    localStorage.setItem("studentName", name);
+    localStorage.setItem("studentRoll", roll);
+
+    window.location.href = "exam.html";
+}
+
+function adminLogin(){
+
+    let password = document.getElementById("adminPassword").value;
+
+    if(password === "admin123"){
+        alert("Admin Panel will be available in the next version.");
+    }else{
+        alert("Incorrect Admin Password.");
+    }
+
+}
