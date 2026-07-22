e5a7601ae10d8d57fa017aab6c9db41cfa525e64
royrohitkumar665-lab <royrohitkumar665@gmail.com>
2026-07-22 11:56:08 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,22 +1,29 @@
-const questions = [
-{
-question: "भारत की राजधानी क्या है?",
-options: ["मुंबई", "कोलकाता", "नई दिल्ली", "चेन्नई"],
-answer: 2
-},
-{
-question: "2 + 2 = ?",
-options: ["3", "4", "5", "6"],
-answer: 1
-},
-{
-question: "RRB का पूर्ण रूप क्या है?",
-options: [
-"Railway Recruitment Board",
-"Road Recruitment Board",
-"Rail Recruitment Bureau",
-"Railway Road Board"
-],
-answer: 0
+let questions = JSON.parse(localStorage.getItem("questionBank"));
+
+if (!questions || questions.length === 0) {
+
+    questions = [
+        {
+            question: "भारत की राजधानी क्या है?",
+            options: ["मुंबई", "कोलकाता", "नई दिल्ली", "चेन्नई"],
+            answer: 2
+        },
+        {
+            question: "2 + 2 = ?",
+            options: ["3", "4", "5", "6"],
+            answer: 1
+        },
+        {
+            question: "RRB का पूर्ण रूप क्या है?",
+            options: [
+                "Railway Recruitment Board",
+                "Road Recruitment Board",
+                "Rail Recruitment Bureau",
+                "Railway Road Board"
+            ],
+            answer: 0
+        }
+    ];
+
+    localStorage.setItem("questionBank", JSON.stringify(questions));
 }
-];
